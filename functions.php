c70b67e31b84fafc96cb301e9a4719705fac4b38
Roshan <rkhudoyberdiev@gmail.com>
2016-12-26 16:50:40 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 function wp_last_modified_meta() {
 	$date = '';
 	if(is_single() || is_page() && !(is_front_page())) {
-		$date = get_the_modified_date('F j, Y');
+		$date = get_the_modified_date('l, F j, Y, g:i a');
 		$meta = '<meta name="revised" content="%s" />' . "\n";
 		if($date)
 			$stuff = sprintf($meta, $date);
