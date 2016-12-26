9f0eef433d70a21a81aea993de84faa5faa3bd63
Roshan <rkhudoyberdiev@gmail.com>
2016-12-26 16:45:17 -0500

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,15 @@
+//Add meta revised tag for updating the Google search result dates
+function wp_last_modified_meta() {
+	$date = '';
+	if(is_single() || is_page() && !(is_front_page())) {
+		$date = get_the_modified_date('F j, Y');
+		$meta = '<meta name="revised" content="%s" />' . "\n";
+		if($date)
+			$stuff = sprintf($meta, $date);
+		else
+			$stuff = '';
+	}
+	echo $stuff;
+}
+
+add_action('wp_head', 'wp_last_modified_meta', 0);
